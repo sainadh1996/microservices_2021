53b5564c5a1e14ff2bd74f840a4597e020863fff
sainadh1996 <sainadh.sirinomula17@gmail.com>
2021-09-29 10:30:27 -0700

    changes for JIRA-102

--- src/java/sample.java
+++ src/java/sample.java
@@ -1,5 +1,6 @@
 class sample{
          public static void main(String args[]){
 		 System.out.println("sample java web app project");
+                  System.out.println("dev02 code changes");
 		 }
 }
