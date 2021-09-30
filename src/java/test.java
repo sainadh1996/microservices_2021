c69bb28a32201009361a1a51b339a942e727e7f1
sainadh1996 <sainadh.sirinomula17@gmail.com>
2021-09-30 07:54:41 -0700

    new feature change-sprint-1

---
+++ src/java/test.java
@@ -0,0 +1,6 @@
+class test{
+         public static void main(String args[]){
+		 System.out.println("sample java web app project");
+                  System.out.println("dev02 code changes");
+		 }
+}
