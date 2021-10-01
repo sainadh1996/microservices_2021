7bc05a3f5f9680d37040be431f9af7128393eaf6
sainadh1996 <sainadh.sirinomula17@gmail.com>
2021-10-01 07:57:38 -0700

    sprint-2 change

--- src/java/test.java
+++ src/java/test.java
@@ -1,6 +1,7 @@
 class test{
          public static void main(String args[]){
 		 System.out.println("sample java web app project");
-                  System.out.println("dev02 code changes");
+                  System.out.println("dev02 code changes merge update on jira-102");
+                  System.out.println("senior developer changes");
 		 }
 }
