9ba4a1b46a4eec48b3b59d9f3c5297c9ef807074
sainadh1996 <90593428+sainadh1996@users.noreply.github.com>
2021-09-30 20:13:21 -0700

    Revert "21.2 release"

--- src/java/sample.java
+++ src/java/sample.java
@@ -1,6 +1,3 @@
 class sample{
-         public static void main(String args[]){
-		 System.out.println("sample java web app project");
-                  System.out.println("dev02 code changes");
-		 }
+
 }
